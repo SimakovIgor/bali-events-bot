98fa23c4fe59edd2fbe9f1ba88cd012b487c4994
Igor Simakov <i.simakov@banki.ru>
2024-02-09 11:49:39 +0800

    Initial commit

---
+++ src/main/java/com/bali/events/balievents/BaliEventsApplication.java
@@ -0,0 +1,13 @@
+package com.bali.events.balievents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaliEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaliEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bali/events/balievents/BaliEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bali.events.balievents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaliEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
