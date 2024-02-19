5a813272e3b099d868f59cadc94d6fadefb1a895
Andrey Antonov <akvilona@list.ru>
2024-02-19 17:27:22 +0300

    add application.yml add logic telegramBot

--- telegram-bot/src/main/java/com/example/telegrambot/service/InlineKeyboardMarkupUtil.java
+++ telegram-bot/src/main/java/com/example/telegrambot/service/InlineKeyboardMarkupUtil.java
@@ -4,12 +4,14 @@
 
 package com.example.telegrambot.service;
 
+import lombok.experimental.UtilityClass;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@UtilityClass
 public class InlineKeyboardMarkupUtil {
     public static InlineKeyboardMarkup createInlineKeyboardMarkup(String buttonText) {
         InlineKeyboardMarkup markupInline = new InlineKeyboardMarkup();
