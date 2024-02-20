dfe2293de7ded8d632f53ddf4c383d808ed5542a
Igor Simakov <i.simakov@banki.ru>
2024-02-21 01:23:34 +0800

    1. Исправил прохождение тестов 2. Добавил тест контейнерсы

--- data-scrapper/src/test/java/com/bali/events/balievents/ApplicationTests.java
+++ data-scrapper/src/test/java/com/bali/events/balievents/ApplicationTests.java
@@ -1,10 +1,22 @@
 package com.bali.events.balievents;
 
+import com.bali.events.balievents.initializer.PostgreSqlInitializer;
+import com.bali.events.balievents.service.SchedulerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {
+    PostgreSqlInitializer.class
+})
+@ActiveProfiles("test")
 class ApplicationTests {
+
+    @MockBean
+    protected SchedulerService schedulerService;
 
     @Test
     void contextLoads() {
--- telegram-bot/src/test/java/com/example/telegrambot/ApplicationTests.java
+++ telegram-bot/src/test/java/com/example/telegrambot/ApplicationTests.java
@@ -1,9 +1,14 @@
 package com.example.telegrambot;
 
+import com.example.telegrambot.initializer.PostgreSqlInitializer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(initializers = {
+    PostgreSqlInitializer.class
+})
 class ApplicationTests {
 
     @Test
