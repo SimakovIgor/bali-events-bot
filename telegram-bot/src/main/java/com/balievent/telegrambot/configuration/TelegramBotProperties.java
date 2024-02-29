27562c397452f44b4782e1b3c50fc355b4173dd8
Igor Simakov <i.simakov@banki.ru>
2024-02-29 15:11:06 +0800

    added properties config for bot

---
+++ telegram-bot/src/main/java/com/balievent/telegrambot/configuration/TelegramBotProperties.java
@@ -0,0 +1,13 @@
+package com.balievent.telegrambot.configuration;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "com.balievent.bot")
+public class TelegramBotProperties {
+    private String username;
+    private String token;
+}
