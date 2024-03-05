31561c9949329f0a5b4a619a5337ead62fc648eb
Igor Simakov <i.simakov@banki.ru>
2024-03-05 18:15:21 +0800

    Disable tests

--- data-scrapper/src/test/java/com/bali/events/balievents/ApplicationTests.java
+++ data-scrapper/src/test/java/com/bali/events/balievents/ApplicationTests.java
@@ -2,6 +2,7 @@ package com.bali.events.balievents;
 
 import com.bali.events.balievents.initializer.PostgreSqlInitializer;
 import com.bali.events.balievents.service.SchedulerService;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -11,6 +12,7 @@ import org.springframework.test.context.ContextConfiguration;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ContextConfiguration(initializers = PostgreSqlInitializer.class)
 @ActiveProfiles("test")
+@Disabled
 class ApplicationTests {
 
     @MockBean
